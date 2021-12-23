25729fdfa6177690713dc9215b4ea44f42c552db
shizeyu-ls <shizeyu-ls@bestpay.com.cn>
2021-12-23 20:03:42 +0800

    第一次提交

---
+++ lib/staff_manage_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+class StaffManagerPage extends StatefulWidget{
+  const StaffManagerPage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return StaffManagePageState();
+  }
+  
+}
+
+class StaffManagePageState extends State<StaffManagerPage>{
+  @override
+  Widget build(BuildContext context) {
+      return Column(
+          
+      );
+  }
+  
+}
